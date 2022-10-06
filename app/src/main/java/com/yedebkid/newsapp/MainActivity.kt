b4413c07fe151faff14294246f7cf83fdcef0f0d
Debasu <bayleyegn100@gmail.com>
2022-10-06 18:04:10 -0400

    Adds Fragments, Main Activity, menu and navigatio.

---
+++ app/src/main/java/com/yedebkid/newsapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.yedebkid.newsapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.yedebkid.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
